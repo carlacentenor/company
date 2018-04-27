cc55bf00bb1fc937f19ae69f2a3fef253c4c1cba
carlacentenor <carlacentenor@gmail.com>
2018-04-27 16:37:00 -0500

    cambios mobile

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+$(document).ready(function() {
+ 
+  if ((screen.width<=768)) {
+    
+    $('.menu-hide').addClass("hide-header");
+  }
+  else {
+    $('.menu-hide').removeClass("hide-header");
+  }
+})
+
 $('#box-1').mouseenter(function () {
   $('#box-1').addClass('box-hover').fadeIn('2000');
 });
@@ -60,38 +71,76 @@ $('#box-4').on('click', function () {
   $('#text-box-1').hide();
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
   $('.slider-client').slick({
     dots: true,
-  infinite: true,
-  speed: 300,
-  autoplay:3000,
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
+    infinite: true,
+    speed: 300,
+    autoplay: 3000,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
       }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
+    ]
   });
 });
+
+
+// Efecto Menu Mobile
+$('#menu-mobile').on('click', function () {
+  $('.menu-mobile-blue').css('overflow', 'visible');
+  $('.menu-mobile-blue').css('width', '100%');
+  $('.menu-mobile-blue').css('height', '100%');
+})
+
+$('.btn-close').on('click', function () {
+  $('.menu-mobile-blue').css('overflow', 'hidden');
+  $('.menu-mobile-blue').css('width', '0%');
+  $('.menu-mobile-blue').css('height', '0%');
+})
+
+// Efecto Menú oculto
+
+
+  $('.menu-hide').hide();
+  $('.menu-visible').show();
+
+   $(document).on('scroll',function(){
+     let size = $(document).scrollTop();
+            if(size <= 50){
+             $('.menu-hide').hide();
+              $('.menu-visible').addClass("d-md-block");
+              
+              $('.menu-visible').show("slow");
+              $('header').css("padding-bottom", "15px");
+            }
+            if(size > 50){
+              $('.menu-hide').show("slow");
+        
+              $('.menu-visible').hide();
+              $('header').css("padding-bottom", "0px");
+              $('.menu-visible').removeClass("d-md-block");
+            }
+   });
+  
